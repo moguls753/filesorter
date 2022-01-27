1a91060a0e644711beea6cf82847aa79cca71c13
Eike <eike.rackwitz@mail.de>
2022-01-27 22:31:09 +0100

    mkdirs and move files into

--- filesorter.py
+++ filesorter.py
@@ -1,8 +1,17 @@
 # a filesorter to sort all incoming files to ~/Downloads
+import shutil
 import os
+from pathlib import Path
 
 os.chdir("/home/eike/Downloads")
 for file in os.listdir():
     if os.path.isdir("/home/eike/Downloads/" + file):
         continue
-    print(file)
+    if not os.path.isdir("/home/eike/Downloads/"+os.path.splitext(file)[1][1:]):
+        os.mkdir("/home/eike/Downloads/"+os.path.splitext(file)[1][1:])
+        print(os.path.splitext(file)[1][1:]+" created")
+        shutil.move("/home/eike/Downloads/"+file, "/home/eike/Downloads/"+os.path.splitext(file)[1][1:]+"/"+file)
+        print(file + " moved to " + os.path.splitext(file)[1][1:])
+    else:
+        shutil.move("/home/eike/Downloads/"+file, "/home/eike/Downloads/"+os.path.splitext(file)[1][1:]+"/"+file)
+        print(file + " moved to " + os.path.splitext(file)[1][1:])
